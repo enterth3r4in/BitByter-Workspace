42c0188698960db5b322a6ae3a430e4df7b8b131
IntoTheRain <intotherain.ccgames@gmail.com>
2017-01-16 17:55:38 -0500

    Initial Commit
    Published and committing all the random shit from libgdx

---
+++ LIBGDX/desktop/src/net/ccgames/bb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.ccgames.bb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.ccgames.bb.MainApplication;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainApplication(), config);
+	}
+}
